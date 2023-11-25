daa87b999a9a247c5d6ea33975fcdcf10a1ad70b
Alessio Nobile <annobile@amazon.nl>
2023-11-25 14:08:15 +0100

    fix destroy script

--- destroy.sh
+++ destroy.sh
@@ -9,10 +9,11 @@ prefix () {
 
 # Delete FrontendStack
 echo -e "\n$(prefix)[$FrontendStack] Initiating deletion.\n"
+frontend=$(aws cloudformation delete-stack --stack-name $FrontendStack --region us-east-1 --output text)
 spin='-\|/'
 i=0
 while true; do
-    frontendStatus=$(aws cloudformation delete-stack --stack-name $FrontendStack --region us-east-1 --output text)
+    frontendStatus=$(aws cloudformation describe-stacks --stack-name $FrontendStack --query 'Stacks[0].StackStatus' --region us-east-1 --output text)
     if [[ "$frontendStatus" == "DELETE_COMPLETE" ]]; then
         break
     fi
@@ -22,10 +23,11 @@ echo -e "\n$(prefix)[$FrontendStack] Deleted\n"
 
 # Delete BackendStack
 echo -e "\n$(prefix)[$BackendStack] Initiating deletion.\n"
+backend=$(aws cloudformation delete-stack --stack-name $BackendStack --region $Region --output text)
 spin='-\|/'
 i=0
 while true; do
-    backendStatus=$(aws cloudformation delete-stack --stack-name $BackendStack --region $Region --output text)
+    backendStatus=$(aws cloudformation describe-stacks --stack-name $BackendStack --query 'Stacks[0].StackStatus' --region $Region --output text)
     if [[ "$backendStatus" == "DELETE_COMPLETE" ]]; then
         break
     fi
@@ -35,10 +37,11 @@ echo -e "\n$(prefix)[$BackendStack] Deleted\n"
 
 # Delete SmilePipeline
 echo -e "\n$(prefix)[$StackName Stack] Initiating deletion.\n"
+pipeline=$(aws cloudformation delete-stack --stack-name $StackName --region $Region --output text)
 spin='-\|/'
 i=0
 while true; do
-    pipelineStatus=$(aws cloudformation delete-stack --stack-name $StackName --region $Region --output text)
+    pipelineStatus=$(aws cloudformation describe-stacks --stack-name $StackName --query 'Stacks[0].StackStatus' --region $Region --output text)
     if [[ "$pipelineStatus" == "DELETE_COMPLETE" ]]; then
         break
     fi
